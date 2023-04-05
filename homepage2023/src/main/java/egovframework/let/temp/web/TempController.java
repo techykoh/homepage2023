e0e3eefbc4dd6811dc22a01d03c5720ef172e2a5
Student <Student@DESKTOP-VD2CFFR>
2023-04-05 17:50:29 +0900

    230405 CRUD 생성

--- homepage2023/src/main/java/egovframework/let/temp/web/TempController.java
+++ homepage2023/src/main/java/egovframework/let/temp/web/TempController.java
@@ -1,5 +1,7 @@
 package egovframework.let.temp.web;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import egovframework.let.temp.service.TempService;
 import egovframework.let.temp.service.TempVO;
+import egovframework.let.utl.fcc.service.EgovStringUtil;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 
 // controller란 ? jsp에 값을 주는 역할을 한다.
@@ -18,7 +22,7 @@ public class TempController {
 	@Resource(name = "tempService")
 	private TempService tempService;
 	
-	// 임시데이터 가져오기
+	// 임시데이터 가져오기 (PK값을 이용해서 하나의 값을 가져오기)
 	@RequestMapping(value = "/temp/select.do")
 	public String select(TempVO tempVO, HttpServletRequest request, ModelMap model) throws Exception{
 		// HttpServletRequest 
@@ -26,6 +30,40 @@ public class TempController {
 		TempVO result = tempService.selectTemp(tempVO);
 		model.addAttribute("result", result);
 		return "temp/TempSelect"; // jsp 위치 경로
+	}
+	
+	
+	// 임시데이터 목록 가져오기
+	@RequestMapping(value = "/temp/selectList.do")
+	public String selectList(TempVO tempVO, HttpServletRequest request, ModelMap model) throws Exception{
+		// List : java.util.List
+		List<EgovMap> resultList = tempService.selectTempList(tempVO);
+		model.addAttribute("resultList", resultList);
+		
+		return "temp/TempSelectList";
+	}
+	
+	
+	// 임시데이터 등록/수정
+	@RequestMapping(value = "/temp/tempRegist.do")
+	public String tempRegist(TempVO tempVO, HttpServletRequest request, ModelMap model) throws Exception {
+		TempVO result = new TempVO();
+		// egovframework.let.utl.fcc.service.EgovStringUtil import하세요.
+		if(!EgovStringUtil.isEmpty(tempVO.getTempId())) { // PK값이 있나요?
+			result = tempService.selectTemp(tempVO); // PK값이 있다면, result에 그 값을 담아주세요.
+		}
+		model.addAttribute("result", result); // PK값이 없으면 result는 기본값으로 세팅하세요.
+		
+		return "temp/TempRegist";
+	}
+	
+	
+	// 임시데이터 등록하기
+	@RequestMapping(value = "/temp/insert.do")
+	public String insert(TempVO tempVO, HttpServletRequest request) throws Exception {
+		tempService.insertTemp(tempVO);
+		
+		return "forward:/temp/selectList.do";
 	}
 
 }
--- homepage2023/src/main/webapp/WEB-INF/jsp/temp/TempSelect.jsp
+++ homepage2023/src/main/webapp/WEB-INF/jsp/temp/TempSelect.jsp
@@ -8,10 +8,36 @@
 <head>
 <meta http-equiv="Content-Language" content="ko">
 <title>데이터 가져오기</title>
+<script src="https://code.jquery.com/jquery-latest.min.js"></script>
 </head>
 <body>
-${result.tempId} : ${result.tempVal}
+	${result.tempId} : ${result.tempVal}
+	
+	<div class="box-btn">
+		<c:url var="uptUrl" value="/temp/tempRegist.do">
+			<c:param name="tempId" value="${result.tempId}"/>
+		</c:url>
+		<a href="${uptUrl}">수정</a>
+		
+		<c:url var="delUrl" value="/temp/delete.do">
+			<c:param name="tempId" value="${result.tempId}" />
+		</c:url>
+		<a href="${delUrl}" class="btn-del">삭제</a>
+		
+		<a href="/temp/selectList.do">목록</a>
+	</div>
+	
+	<script>
+	$(document).ready(function(){
+		$(".btn-del").click(function(){
+			if(!confirm("삭제하시겠습니까?")) {
+				return false;
+			}
+		});
+	});
+	</script>
+
 </body>
 </html>
 
-http://localhost/temp/select.do?tempId=1
+<!-- http://localhost/temp/select.do?tempId=1 -->
